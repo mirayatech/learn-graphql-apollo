1502134f4fad1995da0ba038e7cf3bf294d77725
〽️iraya <mirayaabrodi@gmail.com>
2023-08-21 19:23:16 +0200

    feat: mutation - adding and deleting data

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import database from "./database";
 import { typeDefs } from "./schema";
 
 // Define resolvers for GraphQL queries
+// resolvers
 const resolvers = {
   Query: {
     games() {
@@ -41,6 +42,20 @@ const resolvers = {
   Author: {
     reviews(parent) {
       return database.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      database.games = database.games.filter((game) => game.id !== args.id);
+      return database.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      database.games.push(game);
+      return game;
     },
   },
 };
