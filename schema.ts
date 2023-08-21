22bd2aa08b9235abe22966d151990356813cc398
〽️iraya <mirayaabrodi@gmail.com>
2023-08-21 14:08:23 +0200

    feat: create qraphql schema

---
+++ schema.ts
@@ -0,0 +1,33 @@
+/*
+    Main types:
+    - string
+    - int
+    - float
+    - boolean
+    - ID
+*/
+
+export const typeDefs = `#graphql
+ type Game {
+    is: ID!
+    title: String!
+    platform: [String!]!
+}
+
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+}
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+}
+
+type Query {
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+}
+`;
