6e3ba21689a30fa2a40330bd8e3df0730de22e55
〽️iraya <mirayaabrodi@gmail.com>
2023-08-21 15:32:47 +0200

    add:  GraphQL schema to include relationships between Game, Review, and Author types.

--- dist/schema.js
+++ dist/schema.js
@@ -6,22 +6,28 @@
     - boolean
     - ID
 */
-const typeDefs = `#graphql
+export const typeDefs = `#graphql
  type Game {
     id: ID!
     title: String!
-    platform: [String!]!
+    platform: [String!]!    
+    reviews: [Review!]
+
 }
 
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
 }
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
+
 }
 
 type Query {
--- schema.ts
+++ schema.ts
@@ -9,25 +9,34 @@
 
 export const typeDefs = `#graphql
  type Game {
-    is: ID!
+    id: ID!
     title: String!
-    platform: [String!]!
+    platform: [String!]!    
+    reviews: [Review!]
+
 }
 
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
 }
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
+
 }
 
 type Query {
     reviews: [Review]
+    review(id: ID!): Review
     games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author
 }
 `;
