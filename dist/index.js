2ab51e2fa766873f01c1598408a6dbcf7eb4ea3d
〽️iraya <mirayaabrodi@gmail.com>
2023-08-21 15:32:00 +0200

    feat: fetch related data

--- dist/index.js
+++ dist/index.js
@@ -1,75 +1,45 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-// ------------------------------
-const typeDefs = `#graphql
- type Game {
-    id: ID!
-    title: String!
-    platform: [String!]!
-}
-
-type Review {
-    id: ID!
-    rating: Int!
-    content: String!
-}
-type Author {
-    id: ID!
-    name: String!
-    verified: Boolean!
-}
-
-type Query {
-    reviews: [Review]
-    review(id: ID!): Review
-    games: [Game]
-    game(id: ID!): Game
-    authors: [Author]
-    author(id: ID!): Author
-}
-`;
-let games = [
-    { id: "1", title: "Zelda, Tears of the Kingdom", platform: ["Switch"] },
-    { id: "2", title: "Final Fantasy 7 Remake", platform: ["PS5", "Xbox"] },
-    { id: "3", title: "Elden Ring", platform: ["PS5", "Xbox", "PC"] },
-    { id: "4", title: "Mario Kart", platform: ["Switch"] },
-    { id: "5", title: "Pokemon Scarlet", platform: ["PS5", "Xbox", "PC"] },
-];
-let authors = [
-    { id: "1", name: "mario", verified: true },
-    { id: "2", name: "yoshi", verified: false },
-    { id: "3", name: "peach", verified: true },
-];
-let reviews = [
-    { id: "1", rating: 9, content: "lorem ipsum", author_id: "1", game_id: "2" },
-    { id: "2", rating: 10, content: "lorem ipsum", author_id: "2", game_id: "1" },
-    { id: "3", rating: 7, content: "lorem ipsum", author_id: "3", game_id: "3" },
-    { id: "4", rating: 5, content: "lorem ipsum", author_id: "2", game_id: "4" },
-    { id: "5", rating: 8, content: "lorem ipsum", author_id: "2", game_id: "5" },
-    { id: "6", rating: 7, content: "lorem ipsum", author_id: "1", game_id: "2" },
-    { id: "7", rating: 10, content: "lorem ipsum", author_id: "3", game_id: "1" },
-];
-// -------------------------------
+import database from "./database";
+import { typeDefs } from "./schema";
 // Define resolvers for GraphQL queries
 const resolvers = {
     Query: {
         games() {
-            return games;
-        },
-        reviews() {
-            return reviews;
-        },
-        authors() {
-            return authors;
-        },
-        review(_, args) {
-            return reviews.find((review) => review.id === args.id);
+            return database.games;
         },
         game(_, args) {
-            return games.find((game) => game.id === args.id);
+            return database.games.find((game) => game.id === args.id);
+        },
+        authors() {
+            return database.authors;
         },
         author(_, args) {
-            return authors.find((author) => author.id === args.id);
+            return database.authors.find((author) => author.id === args.id);
+        },
+        reviews() {
+            return database.reviews;
+        },
+        review(_, args) {
+            return database.reviews.find((review) => review.id === args.id);
+        },
+    },
+    Game: {
+        reviews(parent) {
+            return database.reviews.filter((r) => r.game_id === parent.id);
+        },
+    },
+    Review: {
+        author(parent) {
+            return database.authors.find((a) => a.id === parent.author_id);
+        },
+        game(parent) {
+            return database.games.find((g) => g.id === parent.game_id);
+        },
+    },
+    Author: {
+        reviews(parent) {
+            return database.reviews.filter((r) => r.author_id === parent.id);
         },
     },
 };
--- index.ts
+++ index.ts
@@ -9,20 +9,38 @@ const resolvers = {
     games() {
       return database.games;
     },
-    reviews() {
-      return database.reviews;
+    game(_, args) {
+      return database.games.find((game) => game.id === args.id);
     },
     authors() {
       return database.authors;
     },
+    author(_, args) {
+      return database.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return database.reviews;
+    },
     review(_, args) {
       return database.reviews.find((review) => review.id === args.id);
     },
-    game(_, args) {
-      return database.games.find((game) => game.id === args.id);
+  },
+  Game: {
+    reviews(parent) {
+      return database.reviews.filter((r) => r.game_id === parent.id);
     },
-    author(_, args) {
-      return database.authors.find((author) => author.id === args.id);
+  },
+  Review: {
+    author(parent) {
+      return database.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return database.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return database.reviews.filter((r) => r.author_id === parent.id);
     },
   },
 };
